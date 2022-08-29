1dc8ee6256e1ca8620dafdf81f27b308cfcf7f26
Santiago Peña Nieto <santip2357@gmail.com>
2022-08-29 01:33:35 -0500

    se termino el fork y la organizacion de los argumentos

--- main.c
+++ main.c
@@ -8,71 +8,41 @@ int main(){
     int flag = 1, pid, i, tube[2];
     while(flag){
         int size = 0;
-        char c, *d = "abd";
-        char * const ar[20];
-        char * const param[] ={"", "", ""};
+        char c, param[10][20];
+        char *const ar[] = {param[0], param[1], param[2], param[3], param[4] , param[5],
+                            param[6], param[7], param[8], param[9], NULL};
         printf("prompt>");
         fflush(stdout);
         scanf("%c", &c);
         while(c - '\n'){
-            //printf("yes1\n");
             i = 0;
             while(c - ' ' && c - '\n'){
                 param[size][i++] = c;
                 scanf("%c", &c);
             }
-            //printf("yes1\n");
             if(i){
                 param[size++][i] = '\0';
-                //printf("%d", size);
-                //ar = d;
-                //ar = param[size];
-                /*printf("1\n");
-                printf("%s", ar);
-                printf("1\n");*/
             }
             if(c - '\n'){
                 scanf("%c", &c);
             }
         }
-        //*(ar + size + 1) = NULL;
-        /*for(i = 1; i <= size; i++){
-            printf("%s ", param[i]);
-        }*/
         if(size){
             if(strcmp("exit", param[0])){
-                //pipe(tube);
+                for(i = 0; i < 10 - size; i++){
+                    param[i + size][0] = '\0';
+                }
                 pid = fork();
-                //printf("yes ..%d\n", pid);
                 if(pid < 0){
                     printf("Error al crear el hijo");
                     return 0;
                 }else if(pid > 0){
                     printf("papa\n");
-                    //close(tube[0]);
-                    //write(tube[1], ar, sizeof(ar));
-                    /*if(strcmp(param[size - 1], "&")){
-                        printf("espero\n");
-                    }*/
                     wait(NULL);
                     printf("fin papa\n");
                 }else{
                     printf("hijo\n");
-                    //close(tube[1]);
-                    //read(tube[0], ar, sizeof(char**));
-                    //printf("%s", *(ar+1));
-                    char *a = "si", *b, *c;
-                    //char *const ar1[] = {"echo", "hola", param[0], NULL};
-                    //ar1[2][2] = 'c';
-                    //ar1[2][3] = '\0';
-                    /**(ar1 + 2) = b;
-                    *(ar1 + 3) = c;
-                    *(ar1 + 4) = NULL;*/
-                    //printf("%s\n", *(ar1 + 4) );
-                    /*for(i = 0; i < size; i++){
-                        printf("%s ", param[i]);
-                    }*/
-                    //execvp(param[0], param);
+                    execvp(ar[0], ar);
                     printf("fin hijo\n");
                     return 0;
                 }
