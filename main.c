b52132320b78943c1b42e30ecef66452db6a48f2
Santiago Peña Nieto <santip2357@gmail.com>
2022-08-31 07:32:02 -0500

    se mejoro la parte de guardar el historial

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ int main(){
         char c, param[10][20];
         printf("prompt>");
         fflush(stdout);
+        //leer entrada
         scanf("%c", &c);
         while(c - '\n'){
             i = 0;
@@ -41,50 +42,53 @@ int main(){
         }
         if(size){
             if(strcmp("exit", param[0])){
-                for(i = 0; i < 10 - size; i++){
-                    strcpy(param[i + size],"");
+                //verificar historial
+                if(!strcmp("!!", param[0])){
+                    if(record){
+                        history = fopen("history.dat", "rb");
+                        fseek(history, -sizeof(param) - sizeof(int), SEEK_END);
+                        fread(&size, sizeof(int), 1, history);
+                        fread(param, sizeof(param), 1, history);
+                        fclose(history);
+                        printf("prompt>");
+                        for(i = 0; i < size; i++){
+                            printf("%s ", param[i]);
+                        }
+                        printf("\n");
+                    }else{
+                        printf("No commands in history.\n");
+                        continue;
+                    }
                 }
+                //crear hijo
                 pid = fork();
                 if(pid < 0){
                     printf("Error al crear el hijo");
                     return 0;
                 }else if(pid > 0){
+                    //papá
                     printf("papa\n");
-                    if(strcmp("!!", param[0])){
-                        record++;
-                    }
+                    record++;
+                    //guardar historial
+                    history = fopen("history.dat", "ab");
+                    fwrite(&size, sizeof(int), 1, history);
+                    fwrite(param, sizeof(param), 1, history);
+                    fclose(history);
+                    //esperar si hay &
                     if(strcmp(param[size - 1], "&")){
-                        wait(NULL);
                         printf("espero\n");
+                        wait(NULL);
                     }
                     printf("fin papa\n");
                 }else{
+                    //hijo
                     printf("hijo\n");
-                    if(!strcmp("!!", param[0])){
-                        if(record){
-                            history = fopen("history.dat", "rb");
-                            fseek(history, -sizeof(param), SEEK_END);
-                            fread(param, sizeof(param), 1, history);
-                            fclose(history);
-                            printf("prompt>");
-                            for(i = 0; i < 10; i++){
-                                printf("%s ", param[i]);
-                            }
-                            printf("\n");
-                        }else{
-                            printf("No commands in history.\n");
-                            return 0;
-                        }
-                    }
-                    history = fopen("history.dat", "ab");
-                    fwrite(param, sizeof(param), 1, history);
-                    fclose(history);
-                    char *const ar[] = {(size ? param[0] : NULL), (size > 1 ? param[1] : NULL),
+                    char *const arg[] = {(size ? param[0] : NULL), (size > 1 ? param[1] : NULL),
                                         (size > 2 ? param[2] : NULL), (size > 3 ? param[3] : NULL),
                                         (size > 4 ? param[4] : NULL) , (size > 5 ? param[5] : NULL),
                                         (size > 6 ? param[6] : NULL), (size > 7 ? param[7] : NULL), 
                                         (size > 8 ? param[8] : NULL), (size > 9 ? param[9] : NULL), NULL};
-                    execvp(ar[0], ar);
+                    execvp(arg[0], arg);
                     printf("fin hijo\n");
                     return 0;
                 }
@@ -93,6 +97,7 @@ int main(){
             }
         }
     }
+    //guardar log (número de comandos en el historial)
     log = fopen("log.txt", "w");
     fprintf(log, "%d", record);
     fclose(log);
