53a768beb06484c089a4de7e10368f5f90a323e3
Santiago Peña Nieto <santip2357@gmail.com>
2022-08-30 22:10:51 -0500

    se implemento el historial

--- main.c
+++ main.c
@@ -4,8 +4,22 @@
 #include <unistd.h>
 #include <string.h>
 
+int abrirHistorial(){
+    int record;
+    FILE *log;
+    log = fopen("log.txt", "r");
+    if(log != NULL){
+        fscanf(log, "%d", &record);
+        fclose(log);
+        return record;
+    }
+    return 0;
+}
+
 int main(){
-    int flag = 1, pid, i, tube[2];
+    int flag = 1, pid, i, tube[2], record;
+    FILE *history, *log;
+    record = abrirHistorial();
     while(flag){
         int size = 0;
         char c, param[10][20];
@@ -30,7 +44,7 @@ int main(){
         if(size){
             if(strcmp("exit", param[0])){
                 for(i = 0; i < 10 - size; i++){
-                    param[i + size][0] = '\0';
+                    strcpy(param[i + size],"");
                 }
                 pid = fork();
                 if(pid < 0){
@@ -38,10 +52,35 @@ int main(){
                     return 0;
                 }else if(pid > 0){
                     printf("papa\n");
-                    wait(NULL);
+                    if(strcmp("!!", param[0])){
+                        record++;
+                    }
+                    if(strcmp(param[size - 1], "&")){
+                        wait(NULL);
+                        printf("espero\n");
+                    }
                     printf("fin papa\n");
                 }else{
                     printf("hijo\n");
+                    if(!strcmp("!!", param[0])){
+                        if(record){
+                            history = fopen("history.dat", "rb");
+                            fseek(history, -sizeof(param), SEEK_END);
+                            fread(param, sizeof(param), 1, history);
+                            fclose(history);
+                            printf("prompt>");
+                            for(i = 0; i < 10; i++){
+                                printf("%s ", param[i]);
+                            }
+                            printf("\n");
+                        }else{
+                            printf("No commands in history.\n");
+                            return 0;
+                        }
+                    }
+                    history = fopen("history.dat", "ab");
+                    fwrite(param, sizeof(param), 1, history);
+                    fclose(history);
                     execvp(ar[0], ar);
                     printf("fin hijo\n");
                     return 0;
@@ -51,6 +90,16 @@ int main(){
             }
         }
     }
+    log = fopen("log.txt", "w");
+    fprintf(log, "%d", record);
+    fclose(log);
     printf("prompt terminated\n");
     return 0;
 }
+
+/*
+for(i = 0; i < 10; i++){
+    printf("%s ", param[i]);
+}
+printf("\n");
+*/
